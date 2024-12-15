6f309e3f9821faff18596cc2bfa42af8376e7ab7
Jude Jeevan Mahendran <judejeevan.g1@gmail.com>
2024-12-15 21:36:32 +0530

    created questions.dart file to store the quiz questions.

---
+++ lib/data/questions.dart
@@ -0,0 +1,7 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+QuizQuestion questionOne = QuizQuestion(
+    question: 'Which programming language is used for Flutter?',
+    answers: ['Java', 'Kotlin', 'Dart', 'Python']);
+
+List<QuizQuestion> questions = [];
